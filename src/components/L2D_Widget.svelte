d591a996d07f19c037bdb16ec0dc59da78e4bb87
結露 <68598198+XketsuroX@users.noreply.github.com>
2026-05-13 19:09:47 +0800

    feat: add Live2D widget resources

---
+++ src/components/L2D_Widget.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    const Source = '/l2d_resource/autoload.js';
+
+    onMount(() => {
+        if(typeof window == 'undefined' || typeof document == 'undefined') return;
+        
+        if(window.innerWidth < 1024) return;
+
+        if(document.querySelector('script[data-l2d-widget]')) return;
+
+        const script = document.createElement('script');
+        script.src = Source;
+        script.async = true;
+        script.defer = true;
+        script.dataset.l2dWidget = "true";
+
+        document.body.appendChild(script);
+
+        return () => {};
+    });
+
+
+</script>
